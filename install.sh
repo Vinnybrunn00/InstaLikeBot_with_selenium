d748f03fc9a3c3eb78311c6ef9d11ef21224c336
Vinícius Bruno <91799009+Vinnybrunn00@users.noreply.github.com>
2021-10-06 01:25:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 apt-get update && apt-get upgrade -y
 python3 -m pip install --upgrade pip
-python3 -m pip install selenium -y
+python3 -m pip install selenium
 clear
 echo "DEPENDENCIAS INSTALADAS COM SUCESSO"
