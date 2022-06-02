533b952898ff93bfb30bd1c742b177f61ed8ded4
Vinícius Bruno <91799009+Vinnybrunn00@users.noreply.github.com>
2022-06-02 20:12:53 -0300

    Update InstaLikeBot.py

--- InstaLikeBot.py
+++ InstaLikeBot.py
@@ -14,81 +14,82 @@ style = f'[italic][bold]{banner}[/]'
 style2 = f'[italic]{banner}[/]'
 style3 = f'[bold]{banner}[/]'
 
-limpar = 'cls' if os.name == 'nt' else 'clear'
-os.system(limpar)
+clean = 'cls' if os.name == 'nt' else 'clear'
+os.system(clean)
 
 class InstaLikeBot:
     def __init__(self):
         self.firefox = Firefox()
         self.console = Console()
         self.url = 'https://instagram.com'
-        self.user = 'result358' # usuario
-        self.passwd = '22v12g25j' # senha
-        self.profile = 'vinnybrunn00' # perfil desejado
+        self.user = '' # user
+        self.passwd = '' # password
+        self.profile = '' # desired profile
 
         if self.user == "":
-            print('[?] - A o campo USUÁRIO está vazio!')
+            print('[?] - The USER field is empty!')
             sys.exit()
 
         elif self.passwd == "":
-            print('[?] - o campo SENHA está vazio!')
+            print('[?] - The PASSWORD field is empty!')
             sys.exit()
 
         elif self.profile == "":
-            print('[?] - O campo PROFILE está vazio')
+            print('[?] - PROFILE field is empty')
             sys.exit()
         time.sleep(2)
 
-    def Abrir_Navegador(self):
+    def Open_Browser(self):
         print(style)
-        navegador = self.firefox
+        navigator = self.firefox
         with self.console.status(f'{self.url}...') as void:
-            navegador.get(self.url)
+            navigator.get(self.url)
             time.sleep(2)
-        os.system(limpar)
+        os.system(clean)
 
-    def Usuario(self):
+    def User(self):
         print(style2)
         try:    
-            usuario = self.firefox.find_element_by_name('username')
-            usuario.click()
-            usuario.send_keys(self.user)
-            senha = self.firefox.find_element_by_name('password')
-            senha.click()
-            senha.send_keys(self.passwd)
-            senha.send_keys(Keys.RETURN)
+            user = self.firefox.find_element_by_name('username')
+            user.click()
+            user.send_keys(self.user)
+            password = self.firefox.find_element_by_name('password')
+            password.click()
+            password.send_keys(self.passwd)
+            password.send_keys(Keys.RETURN)
         except:
-            print('[!] - Elemento não encontrado, ou erro inesperado!')
+            print('[!] - Element not found, or unexpected error!')
             sys.exit()
         time.sleep(2)
-        os.system(limpar)
+        os.system(clean)
 
-    def Perfil_Desejado(self):
+    def Desired_Profile(self):
         print(style3)
         ff = len(self.passwd)
-        ocultar = '*'*ff
+        hide = '*'*ff
         print(f'''
-            ============= Dados Pessoais =============
+            ============= Personal Data =============
             Usuário: [yellow]{self.user}[/]
-            Senha: [green]{ocultar}[/]
+            Senha: [green]{hide}[/]
             ==========================================
             ''')
 
-        with self.console.status('Logando...') as void:
+        with self.console.status('logging in...') as void:
             time.sleep(3)
-        #os.system(limpar)
+        #os.system(clean)
+
         try:
             hastag = f"https://instagram.com/{self.profile}"
-            perfil = self.firefox
-            perfil.get(hastag)
-            with self.console.status(f"Acessando: [green]{hastag}...[/]") as void:
+            profile = self.firefox
+            profile.get(hastag)
+            with self.console.status(f"Accessing: [green]{hastag}...[/]") as void:
                 time.sleep(3)
-            os.system(limpar)
+            os.system(clean)
         except:
             print('Senha incorreta, tente novamente')
             sys.exit()
 
-    def Clickar_na_foto(self):
+    def Click_On_Photo(self):
         print(style)
         try:
             click = self.firefox.find_element_by_class_name('_aagu')
@@ -96,33 +97,33 @@ class InstaLikeBot:
             time.sleep(3)
 
         except:
-            print('[!] - Elemento não encontrado, ou erro inesperado')
+            print('[!] - Element not found, or unexpected error')
             quit()
             sys.exit()
 
         while True:
             #value =+ 1 
-            curtir_fotos = self.firefox.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button')
-            curtir_fotos.click()
-            print("[green][+] Foto curtida! :heavy_check_mark:[/]")
+            like_photos = self.firefox.find_element_by_xpath('/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button')
+            like_photos.click()
+            print("[green][+] Liked photo! :heavy_check_mark:[/]")
             time.sleep(3)
 
             try:
-                proxima_foto = self.firefox.find_element_by_class_name('_aaqg')
-                with self.console.status('Proxima foto...') as void:
-                    proxima_foto.click()
+                next_picture = self.firefox.find_element_by_class_name('_aaqg')
+                with self.console.status('Next photo...') as void:
+                    next_picture.click()
                     time.sleep(2)
-                os.system(limpar)
+                os.system(clean)
 
             except:
-                os.system(limpar)
-                print("\n\n[yellow][!] Todas as fotos curtidas com sucesso! :heavy_check_mark:[/]")
+                os.system(clean)
+                print("\n\n[yellow][!] All photos successfully liked! :heavy_check_mark:[/]")
                 #print(f'[+] Total de fotos curtidas: {value}')
                 self.firefox.quit()
                 break
 
 vini = InstaLikeBot()
-vini.Abrir_Navegador()
-vini.Usuario()
-vini.Perfil_Desejado()
-vini.Clickar_na_foto()
+vini.Open_Browser()
+vini.User()
+vini.Desired_Profile()
+vini.Click_On_Photo()
