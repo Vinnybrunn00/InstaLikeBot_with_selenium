9085083361855c484cbcd0568fef08078a6553fa
Vinícius Bruno <91799009+Vinnybrunn00@users.noreply.github.com>
2023-07-12 23:40:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,131 +1,61 @@
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from rich.console import Console
-from pyfiglet import figlet_format
 from rich import print
-import time
-import sys
-import os
-import requests
+from time import sleep
+import sys, os
 
-#value = 0
+console = Console()
+clear = 'cls' if os.name == 'nt' else 'clear'
 
-banner = figlet_format('InstaLikeBot')
-style = f'[italic][bold]{banner}[/]'
-style2 = f'[italic]{banner}[/]'
-style3 = f'[bold]{banner}[/]'
-
-clean = 'cls' if os.name == 'nt' else 'clear'
-os.system(clean)
-
-class InstaLikeBot:
+class Bot:
     def __init__(self):
-        self.firefox = Firefox()
-        self.console = Console()
+        self.navegador = Firefox()
         self.url = 'https://instagram.com'
-        self.user = 'vinnybrunn00' # user
-        self.passwd = '22v12g25j' # password
-        self.profile = 'tntsportsbr' # desired profile
-
-        if self.user == "":
-            print('[?] - The USER field is empty!')
-            sys.exit()
-
-        elif self.passwd == "":
-            print('[?] - The PASSWORD field is empty!')
-            sys.exit()
-
-        elif self.profile == "":
-            print('[?] - PROFILE field is empty')
-            sys.exit()
-        time.sleep(2)
-
-    def Open_Browser(self):
-        print(style)
-        navigator = self.firefox
-        with self.console.status(f'{self.url}...') as void:
-            navigator.get(self.url)
-            time.sleep(2)
-        os.system(clean)
-
-    def User(self):
-        print(style2)
-        try:    
-            user = self.firefox.find_element_by_name('username')
-            user.click()
-            user.send_keys(self.user)
-            password = self.firefox.find_element_by_name('password')
-            password.click()
-            password.send_keys(self.passwd)
-            password.send_keys(Keys.RETURN)
-        except:
-            print('[!] - Element not found, or unexpected error!')
-            sys.exit()
-        time.sleep(2)
-        os.system(clean)
-
-    def Desired_Profile(self):
-        print(style3)
-        ff = len(self.passwd)
-        hide = '*'*ff
-        print(f'''
-            ============= Personal Data =============
-            Usuário: [yellow]{self.user}[/]
-            Senha: [green]{hide}[/]
-            ==========================================
-            ''')
-
-        with self.console.status('logging in...'):
-            time.sleep(3)
-        #os.system(clean)
-
-        try:
-            hastag = f"https://instagram.com/{self.profile}"
-            code = requests.get(hastag)
-            print(f'Status Code > [green]{code.status_code}[/]')
-            profile = self.firefox
-            profile.get(hastag)
-            with self.console.status(f"Accessing: [green]{hastag}...[/]"):
-                time.sleep(10)
-            os.system(clean)
-        except:
-            print('Senha incorreta, tente novamente')
-            sys.exit()
-
-    def Click_On_Photo(self):
-        print(style)
-        try:
-            click = self.firefox.find_element_by_class_name('_aagw')
-            click.click()
-            time.sleep(3)
-
-        except:
-            print('[!] - Element not found, or unexpected error')
-            quit()
-
+    
+    def getInstagram(self):
+        browser = self.navegador
+        browser.get(self.url)
+        sleep(5)
+        print(f'{self.url} :heavy_check_mark:')
+        
+    def login(self, username, password):
+        user = self.navegador.find_element(By.XPATH, '//input[@name="username"]')
+        user.click()
+        user.send_keys(username)
+        passwd = self.navegador.find_element(By.XPATH, '//input[@name="password"]')
+        passwd.click()
+        passwd.send_keys(password, Keys.RETURN)
+        sleep(5)
+        print('Login Sucess :heavy_check_mark:')
+        
+    def Search(self, hashtag: str):
+        search = self.navegador
+        search.get(f'{self.url}/{hashtag}')
+        sleep(5)
+        print(self.url + f'/{hashtag} :heavy_check_mark:')
+    
+    def getPhoto(self):
+        self.navegador.find_element(By.XPATH, '//div[@class="_aabd _aa8k  _al3l"]').click()
+        print('Click Photo :heavy_check_mark:')
+        
+        count = 0
         while True:
-            value =+ 1 
-            like_photos = self.firefox.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button')
-            like_photos.click()
-            print("[green][+] Liked photo! :heavy_check_mark:[/]")
-            time.sleep(3)
-
+            count+=1
+            self.navegador.find_element(By.XPATH, '//span[@class="_aamw"]').click()
+            #self.navegador.find_element(By.XPATH, '//span[@class="_aamw"]//div[@role="button"]').click()
+            sleep(2)
             try:
-                next_picture = self.firefox.find_element_by_class_name('_aaqg._aaqh')
-                with self.console.status('Next photo...'):
-                    next_picture.click()
-                    time.sleep(2)
-                os.system(clean)
-
+                self.navegador.find_element(By.CLASS_NAME, '_abm0')
+                sleep(2)
             except:
-                os.system(clean)
-                print("\n\n[yellow][!] All photos successfully liked! :heavy_check_mark:[/]")
-                print(f'[+] Total de fotos curtidas: {value}')
-                self.firefox.quit()
-                break
-
-vini = InstaLikeBot()
-vini.Open_Browser()
-vini.User()
-vini.Desired_Profile()
-vini.Click_On_Photo()
+                os.system(clear)
+                print(f'Todas as {count} fotos curtidas com sucesso')
+                sys.exit()
+                
+bot = Bot()
+bot.getInstagram()
+bot.login('username', 'password')
+bot.Search('profile')
+bot.getPhoto()
